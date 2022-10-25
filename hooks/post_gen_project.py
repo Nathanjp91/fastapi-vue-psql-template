58b7af980e4934b56ffc0f0f3852503b9cea3b33
Nathan Perkins <nathanjp91@gmail.com>
2022-10-25 21:44:20 +0100

    reorganizing linting and hook calls

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,3 +1,7 @@
 import subprocess
 
+# initialize git repo
 subprocess.call(['git', 'init'])
+
+# copy .env file to backend: Single source of truth
+subprocess.call(['cp', '.env', 'backend/app/.env'])
--- {{cookiecutter.project_slug}}/backend/app/hooks/update-requirements.sh
+++ {{cookiecutter.project_slug}}/backend/app/hooks/update-requirements.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+cd ./backend/app
 poetry export --without-hashes --format=requirements.txt > requirements.txt
